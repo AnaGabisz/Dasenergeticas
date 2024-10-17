cee3ce390eaaac990832df3204531064374c4fa8
AnaGabisz <anagabriellasouzasilva6@gmail.com-ltda.com.br>
2024-10-17 11:06:36 -0300

    Mudanças de estrutura

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import './App.css';
 import logo from './logo3.png'; // Certifique-se de que o logo está importado
+import { FaUser } from 'react-icons/fa'; // Importando o ícone do usuário
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="logo-title rowdies-bold">Dasenergéticas</h1> {/* Classe para a fonte */}
+        <h1 className="logo-title rowdies-bold">Dasenergéticas</h1>
         <div className="header-top">
           <input
             type="text"
             placeholder="O que você está procurando?"
             className="search-bar"
           />
+          <div className="user-area">
+            <FaUser className="user-icon" />
+            <span className="login-text">Entre ou 
+              Cadastre-se</span>
+          </div>
         </div>
         <img src={logo} className="logo" alt="logo" />
-        <p className="introducao rowdies-regular">
+        <p className="introducao poppins-text">
           Olá, sou Ana Rodrigues, psicanalista, mentora, consultora, e vou guiar você para o seu autoconhecimento.
         </p>
-        <div className="servicos">
-          <h2 className="rowdies-bold">Serviços:</h2> {/* Classe para a fonte */}
+        <div className="servicos poppins-text">
+          <h2>Serviços:</h2>
           <ul>
-            <li>Terapi a Individual: R$ 251,00 por sessão</li>
-            <li>Constelação Familiar: R$ 450,00 (presencial ou online)</li>
-            <li>Constelação Familiar: R$ 648,00 (presencial + 1 sessão online)</li>
+           <li>Terapia Individual: R$ 251,00 por sessão</li>
+           <li>Constelação Familiar: R$ 450,00 (presencial ou online)</li>
+           <li>Constelação Familiar: R$ 648,00 (presencial + 1 sessão online)</li>
           </ul>
         </div>
       </header>
